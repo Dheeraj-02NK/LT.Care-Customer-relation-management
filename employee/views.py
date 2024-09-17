d2d226f19dafd27511670dc0db9d3291de703661
Chaitanya <chaitanyabasavaraju03@gmail.com>
2024-09-17 17:07:08 +0530

    employee modifications

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,22 @@
+from typing import Any
 from django.shortcuts import render
+from .models import employee
+from rest_framework.views import APIView
+from django.views.generic.base import TemplateView
+from django.http import JsonResponse
 
 # Create your views here.
+def emp(request):
+    context = {}
+    context["currentuser"] = request.session["user_data"]
+    return render(request, 'employee/employee.html/',context)
+def profile(request):
+    return render(request, 'profile.html/')
+
+class Viewtkassign(TemplateView):
+    template_name='employee/employee.html'
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        userdata = employee.objects.all()
+        context['userdata']= userdata
+        return context
