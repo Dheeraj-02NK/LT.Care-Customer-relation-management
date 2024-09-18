0f57d20e9038877dc1989c1295c43835b95692a9
Chaitanya <chaitanyabasavaraju03@gmail.com>
2024-09-18 15:07:26 +0530

    employee changes

--- employee/urls.py
+++ employee/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 from .views import Viewtkassign
+from .views import update_ticket_status
+
 
 urlpatterns = [
     # path('emp1/',views.emp, name='emp1'),
     path('profile/',views.profile, name='profile'),
     path('emp1/', Viewtkassign.as_view(), name='employee_page'),
+    
 ]
+
